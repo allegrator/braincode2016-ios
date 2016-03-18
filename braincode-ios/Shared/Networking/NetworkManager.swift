fa251d13595c09ce194f8bbe2cbb034fb424852c
Kacper Harasim <kacper.harasim@polidea.com>
2016-03-18 19:06:09 +0100

    Dependencies added.

---
+++ braincode-ios/Shared/Networking/NetworkManager.swift
@@ -0,0 +1,23 @@
+//
+//  NetworkManager.swift
+//  braincode-ios
+//
+//  Created by Kacper Harasim on 18.03.2016.
+//  Copyright © 2016 Kacper Harasim. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import RxSwift
+
+
+class NetworkManager {
+
+
+    private let baseAddress = "http://10.3.8.27:5000"
+
+    func basicRequest() -> Observable<JSON> {
+
+    }
+
+}
