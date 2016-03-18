698452a6cf6fa7eac482bd58c6abed1c307a581b
Kacper Harasim <kacper.harasim@polidea.com>
2016-03-18 22:16:46 +0100

    Image resizing.

--- braincode-ios/Shared/ImageProcessor/ImageProcessor.swift
+++ braincode-ios/Shared/ImageProcessor/ImageProcessor.swift
@@ -15,7 +15,18 @@ protocol ImageProcessor {
 
 class BraincodeImageProcessor: ImageProcessor {
 
+    let resizingValue = CGSize(width: 800, height: 800)
     func processImage(image: UIImage) -> UIImage {
-        return image
+        return self.resizeImage(image, toSize: self.resizingValue)
+    }
+
+    private func resizeImage(image: UIImage, toSize newSize: CGSize) -> UIImage {
+
+        UIGraphicsBeginImageContext(newSize)
+        image.drawInRect(CGRectMake(0, 0, newSize.width, newSize.height))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return newImage
+
     }
 }
